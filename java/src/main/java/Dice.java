4179cb7e56817061de9a97072b70bd92e86e26eb
gitanitraj <anitra.com@gmail.com>
2026-04-16 18:00:49 -0400

    Added while look

--- java/src/main/java/Dice.java
+++ java/src/main/java/Dice.java
@@ -15,4 +15,18 @@ public class Dice {
     public int getValue() {
         return value;
     }
+
+public class Main {
+    public static void main(String[] args) {
+        Dice dice = new Dice();
+        dice.roll();
+        System.out.println("Rolled: " + dice.getValue());
+    }
 }
+
+while (true) {
+    int roll = dice.roll();
+    System.out.println("You rolled: " + roll);
+}
+}
+
