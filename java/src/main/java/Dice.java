bddee1c244a3a448b968ac0711a258fb8b73c9c9
gitanitraj <anitra.com@gmail.com>
2026-04-16 16:38:09 -0400

    Added roll and result

--- java/src/main/java/Dice.java
+++ java/src/main/java/Dice.java
@@ -1,4 +1,18 @@
+import java.util.Random;
+
 public class Dice {
+    private int value;
+    private Random random;
 
+    public Dice() {
+        random = new Random();
+    }
 
+    public void roll() {
+        value = random.nextInt(6) + 1; // 1–6
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
